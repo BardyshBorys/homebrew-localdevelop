2fe39eb5d6f84ebd2ebb99c0b47c66ec45a3a340
borys.bardyshabacusinsights.com <bardyshborys@gmail.com>
2021-04-05 13:58:48 +0300

    add formulas

--- sipy.rb
+++ sipy.rb
@@ -8,14 +8,7 @@ class Sipy < Formula
   url "git@github.com:BardyshBorys/homebrew-localdevelop.git"
   license "MIT"
   head "git@github.com:BardyshBorys/homebrew-localdevelop.git"
- version "#$TAG"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f576b76fa2209abfa8b0923f378318199af89ddfb4915150243d1d7e73543ac0"
-    sha256 cellar: :any_skip_relocation, big_sur:       "24d61894796f80e46465356b2a45a85861789a430019efd07952c108018eaba4"
-    sha256 cellar: :any_skip_relocation, catalina:      "cd96e71e3eaf910f6d70dd78a02ccd21048302a16eaaf2fbfedb15712d469752"
-    sha256 cellar: :any_skip_relocation, mojave:        "cbf4b82ab3f8f26ac6c7363a411aa3b692b9f2464a390e634dc5b4f61f2da0b7"
-  end
+  version "#$TAG"
 
   depends_on "python@3.9"
 
@@ -45,4 +38,18 @@ class Sipy < Formula
     sha256 "19188f96923873c92ccb987120ec4acaa12f0461fa9ce5d3d0772bc965a39e08"
   end
 
+  def install
+    venv = virtualenv_create(libexec, Formula["python@3.9"].opt_bin/"python3")
+    venv.pip_install resources
+    venv.pip_install buildpath
+
+    # Make the Homebrew site-packages available in the interpreter environment
+    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    ENV.prepend_path "PYTHONPATH", HOMEBREW_PREFIX/"lib/python#{xy}/site-packages"
+    ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    combined_pythonpath = ENV["PYTHONPATH"] + "${PYTHONPATH:+:}$PYTHONPATH"
+    %w[bpdb bpython].each do |cmd|
+      (bin/cmd).write_env_script libexec/"bin/#{cmd}", PYTHONPATH: combined_pythonpath
+    end
+  end
 end
