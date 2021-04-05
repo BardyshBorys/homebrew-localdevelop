d1d874c43b32b81a79417af868f21459723b5633
borys.bardyshabacusinsights.com <bardyshborys@gmail.com>
2021-04-05 15:34:29 +0300

    add formulas

--- sipy.rb
+++ sipy.rb
@@ -5,6 +5,7 @@ class Sipy < Formula
   homepage "https://github.com/BardyshBorys/ScienceBundleMacOS"
   url "https://github.com/BardyshBorys/ScienceBundleMacOS"
   head "git@github.com:BardyshBorys/ScienceBundleMacOS.git"
+  version "#$TAG"
 
   resource "setuptools" do
     url "https://files.pythonhosted.org/packages/1a/04/d6f1159feaccdfc508517dba1929eb93a2854de729fa68da9d5c6b48fa00/setuptools-39.2.0.zip"
