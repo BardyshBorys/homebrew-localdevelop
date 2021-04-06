cfb8586af030d12fe2bf1de79652818e010c5435
borys.bardyshabacusinsights.com <bardyshborys@gmail.com>
2021-04-06 13:06:34 +0300

    add formulas

--- sipy@2021.04.05.rb
+++ sipy@2021.04.05.rb
@@ -4,7 +4,7 @@ class SipyAT20210405 < Formula
   include Language::Python::Virtualenv
   desc "python packages related to scientific work"
   homepage "https://github.com/BardyshBorys/ScienceBundleMacOS"
-  url  "git@github.com:BardyshBorys/ScienceBundleMacOS.git", :using => :git, :branch => "main", :tag => "$TAG"
+  head  "git@github.com:BardyshBorys/ScienceBundleMacOS.git", :using => :git, :branch => "main", :tag => "$TAG"
   version "$TAG"
   sha256 "54c1f67fb1672908032d060020640f6a1e20057c7c31bb62a3f4791a3fee8cba"
 
