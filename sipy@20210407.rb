9bf44667bb2671a0c0205c479dda4ea52967aebe
borys.bardyshabacusinsights.com <bardyshborys@gmail.com>
2021-04-06 12:57:43 +0300

    add formulas

--- sipy@20210407.rb
+++ sipy@20210407.rb
@@ -1,6 +1,6 @@
 $TAG = "2021.04.07"
 
-class SipyAT20210406 < Formula
+class SipyAT20210407 < Formula
   include Language::Python::Virtualenv
   desc "python packages related to scientific work"
   homepage "https://github.com/BardyshBorys/ScienceBundleMacOS"
